e2ede68a9795561e12fd57313ed5cdb95d577978
Yevhen Shved <Dok6857@gmail.com>
2024-07-03 11:23:07 +0300

    refactor

--- crm/src/app/page.tsx
+++ crm/src/app/page.tsx
@@ -1,11 +1,13 @@
-import StatusLabel from "./components/status-label";
-
+import StatusLabel, { Status } from './components/status-label';
 
 export default function Home() {
   return (
     <main>
       <h1>Home page</h1>
-      <StatusLabel>
+      <StatusLabel status={Status.Active}>Active</StatusLabel>
+      <StatusLabel status={Status.NotActive}>NotActive</StatusLabel>
+      <StatusLabel status={Status.Pending}>Pending</StatusLabel>
+      <StatusLabel status={Status.Suspended}>Suspended</StatusLabel>
     </main>
   );
 }
