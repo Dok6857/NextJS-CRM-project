e26224e450fc234acfcd613f94463f67917044d1
Yevhen Shved <Dok6857@gmail.com>
2024-07-16 19:11:15 +0300

    move folders

--- crm/src/app/{admin}/companies/page.tsx
+++ crm/src/app/{admin}/companies/page.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import Header from '../components/header';
-import Toolbar from '../components/tollbar';
-import SearchInput from '../components/search-input';
-import AddCompanyButton from '../components/add-company-button';
-import CompanyTable from '../components/company-table';
-import CompanyRow from '../components/company-row';
-import StatusLabel, { Status } from '../components/status-label';
+import Header from '../../components/header';
+import Toolbar from '../../components/tollbar';
+import SearchInput from '../../components/search-input';
+import AddCompanyButton from '../../components/add-company-button';
+import CompanyTable from '../../components/company-table';
+import CompanyRow from '../../components/company-row';
+import StatusLabel, { Status } from '../../components/status-label';
 
 export interface PageProps {}
 
