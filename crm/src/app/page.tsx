56ef9d16afb6507813952156c9020e52f0efe529
Dok6857 <Dok6857@gmail.com>
2024-07-24 13:34:29 +0300

    delete my test

--- crm/src/app/page.tsx
+++ crm/src/app/page.tsx
@@ -1,12 +1,10 @@
 import AddCompanyButton from './components/add-company-button';
-import Colors from './components/colors';
 
 export default function Home() {
   return (
     <main>
       <h1 className="text-xl">Home page</h1>
       <AddCompanyButton />
-      <Colors />
     </main>
   );
 }
